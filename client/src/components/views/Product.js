7cbb8dd05470afdd31e92b850ed40dfca7067927
Casey <caseywilcox@outlook.com>
2021-10-10 18:30:39 -0700

    Updated Product

--- client/src/components/views/Product.js
+++ client/src/components/views/Product.js
@@ -35,23 +35,82 @@ function Product() {
     }
   };
 
+  const purchaseProduct = async (id, price) => {
+    try {
+      if (window.ethereum) {
+        console.log("Transaction started");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const marketplaceContract = new ethers.Contract(
+          marketplaceAddress,
+          marketplaceABI,
+          signer
+        );
+
+        console.log(price);
+        console.log(ethers.utils.formatEther(price));
+
+        const createTx = await marketplaceContract.purchaseProduct(id, {
+          gasLimit: 300000,
+          value: ethers.utils.parseUnits(
+            ethers.utils.formatEther(price),
+            "ether"
+          ),
+        });
+
+        await createTx.wait();
+
+        console.log(
+          "Transaction completed for: " +
+            ethers.utils.formatEther(
+              ethers.utils.parseUnits(price.toString(), "ether")
+            ) +
+            " ETH"
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     fetchProduct();
   }, []);
 
   return (
     <section id="product">
-      <h1>Product: {id}</h1>
-      {product && (
-        <>
-          <p>{product.name}</p>
-          <p>{ethers.utils.formatEther(product.price)} ETH</p>
-          <p>{product.id.toString()}</p>
-          <p>
-            {product.seller.slice(0, 6)}...{product.seller.slice(-4)}
-          </p>
-        </>
-      )}
+      <header>
+        <div className="product-container">
+          {product && (
+            <>
+              <div className="product-header">
+                <h2>{product.name}</h2>
+                <p>
+                  {product.seller.slice(0, 6)}...{product.seller.slice(-4)}
+                </p>
+              </div>
+              <p className="pricing">
+                {ethers.utils.formatEther(product.price)} ETH
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est,
+                pretium quis commodo lobortis integer amet adipiscing sagittis,
+                etiam. Hendrerit vel dolor, orci facilisi ornare morbi
+                adipiscing.
+              </p>
+            </>
+          )}
+          <button
+            onClick={() => {
+              purchaseProduct(product.id, product.price);
+            }}
+          >
+            Purchase
+          </button>
+        </div>
+      </header>
     </section>
   );
 }
