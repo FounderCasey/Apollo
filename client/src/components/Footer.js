26e9bb95e77eac90e1d35d6da16ab7c8b805eb1c
Casey Wilcox <caseywilcox@outlook.com>
2022-01-11 12:49:42 -0800

    minor fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,6 +10,10 @@ function Footer() {
           <a href="https://imcasey.com" target="_blank">
             0xCasey
           </a>
+          &{" "}
+          <a href="https://github.com/gtlpanda" target="_blank">
+            0xCarlos
+          </a>
         </p>
         <p>
           Please use the <span>Rinkeby</span> testnet.
